5fe1c963773129c74c4ec98093d459eaa1b1b667
MVPlauda <plaudadev@gmail.com>
2023-04-29 01:33:33 -0300

    somando

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿internal class program
+{
+    private static void Main(string[] args)
+    {
+        float num1, num2, soma;
+
+        Console.Write("Digite um numero: ");
+        num1 = float.Parse(Console.ReadLine());
+        Console.Write("Digite outro numero: ");
+        num2 = float.Parse(Console.ReadLine());
+
+        soma = num1 + num2;
+
+        Console.WriteLine($"A soma entre os 2 numero é: {soma}");
+
+    }
+}
+
+
+
+
+
+
+
+
